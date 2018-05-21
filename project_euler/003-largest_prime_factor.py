86f4f5497b76869b76166675857c659d3954d0d6
tsumo <tsumo@users.noreply.github.com>
2018-05-21 17:34:45 +0300

    project euler problem 5 optimized
    Also some comment formatting

--- project_euler/003-largest_prime_factor.py
+++ project_euler/003-largest_prime_factor.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-# Largest prime factor
-# Problem 3
+"""
+Largest prime factor
+Problem 3
 
-# The prime factors of 13195 are 5, 7, 13 and 29.
+The prime factors of 13195 are 5, 7, 13 and 29.
 
-# What is the largest prime factor of the number 600851475143 ?
-
-
-#=====================
+What is the largest prime factor of the number 600851475143 ?
+"""
 
 
 import time
--- project_euler/005-smallest_multiple.py
+++ project_euler/005-smallest_multiple.py
@@ -12,26 +12,41 @@ numbers from 1 to 20?
 """
 
 
-#=====================
-
-
 import time
 from datetime import timedelta
 
 
-def sm(n):
-    i = n + 1
-    while True:
-        if check(i, n):
-            return i
-        i += 1
+def problem5(n):
+    """Computes lowest common multiple for integers from 1 to n."""
+    return lcm_list(range(1, n + 1))
 
 
-def check(x, n):
-    for i in range(1, n + 1):
-        if x % i != 0:
-            return False
-    return True
+def gcd(x, y):
+    """Find greatest common denominator."""
+    while y:
+        x, y = y, x % y
+    return x
+
+
+def gcd_list(lst):
+    """Find gcd for any number of integers."""
+    res = lst[0]
+    for i in lst[1:]:
+        res = gcd(res, i)
+    return res
+
+
+def lcm(x, y):
+    """Find lowest common multiple."""
+    return int((x * y) / gcd(x, y))
+
+
+def lcm_list(lst):
+    """Find lcm for any number of integers."""
+    res = lst[0]
+    for i in lst[1:]:
+        res = lcm(res, i)
+    return res
 
 
 def time_and_print(func, arg, expectation):
@@ -43,10 +58,10 @@ def time_and_print(func, arg, expectation):
     RED = "\033[0;31m"
     END = "\033[0m"
     t1 = time.process_time()
-    res = func(arg)
+    res = func(*arg)
     t2 = time.process_time() - t1
     if res == expectation:
-        print(GREEN, func.__name__, END, "for", arg)
+        print(GREEN, func.__name__, END, "for", *arg)
     else:
         print(RED, func.__name__, END)
     print("    ", rpad(res), "vs", lpad(expectation), "time:", timedelta(seconds=t2))
@@ -61,6 +76,35 @@ def lpad(i, n = 8):
     return str(i).ljust(n)
 
 
-time_and_print(sm, 10, 2520)
-time_and_print(sm, 20, 232792560)
+time_and_print(gcd, [42, 56], 14)
+time_and_print(gcd, [54, 24], 6)
+
+time_and_print(gcd_list, [(1, 2, 3)], 1)
+time_and_print(gcd_list, [(2, 4, 6, 8)], 2)
+
+time_and_print(lcm, [5, 2], 10)
+
+time_and_print(lcm_list, [(5, 2, 1)], 10)
+time_and_print(lcm_list, [(2, 7, 3, 9, 4)], 252)
+
+time_and_print(problem5, [1], 1)
+time_and_print(problem5, [2], 2)
+time_and_print(problem5, [3], 6)
+time_and_print(problem5, [4], 12)
+time_and_print(problem5, [5], 60)
+time_and_print(problem5, [6], 60)
+time_and_print(problem5, [7], 420)
+time_and_print(problem5, [8], 840)
+time_and_print(problem5, [9], 2520)
+time_and_print(problem5, [10], 2520)
+time_and_print(problem5, [11], 27720)
+time_and_print(problem5, [12], 27720)
+time_and_print(problem5, [13], 360360)
+time_and_print(problem5, [14], 360360)
+time_and_print(problem5, [15], 360360)
+time_and_print(problem5, [16], 720720)
+time_and_print(problem5, [17], 12252240)
+time_and_print(problem5, [18], 12252240)
+time_and_print(problem5, [19], 232792560)
+time_and_print(problem5, [20], 232792560)
 
