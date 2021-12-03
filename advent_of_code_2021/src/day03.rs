cbde4e220e6f45eb76d4cbb1f5181abe00f1031b
tsumo <tsumo@users.noreply.github.com>
2021-12-03 21:30:29 +0300

    aoc 2021 day 3.1 single pass

--- advent_of_code_2021/src/day03.rs
+++ advent_of_code_2021/src/day03.rs
@@ -13,54 +13,58 @@ pub fn first() -> i32 {
 
   let numbers = lines.iter().map(|s| i32::from_str_radix(s, 2).unwrap());
 
-  let r1 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 11));
-  let r2 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 10));
-  let r3 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 9));
-  let r4 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 8));
-  let r5 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 7));
-  let r6 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 6));
-  let r7 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 5));
-  let r8 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 4));
-  let r9 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 3));
-  let r10 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 2));
-  let r11 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 1));
-  let r12 = numbers.clone().fold(0, |acc, c| acc - get_bit_at(c, 0));
+  let mut r = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-  let g1 = if r1 > 0 { 1 } else { 0 };
-  let g2 = if r2 > 0 { 1 } else { 0 };
-  let g3 = if r3 > 0 { 1 } else { 0 };
-  let g4 = if r4 > 0 { 1 } else { 0 };
-  let g5 = if r5 > 0 { 1 } else { 0 };
-  let g6 = if r6 > 0 { 1 } else { 0 };
-  let g7 = if r7 > 0 { 1 } else { 0 };
-  let g8 = if r8 > 0 { 1 } else { 0 };
-  let g9 = if r9 > 0 { 1 } else { 0 };
-  let g10 = if r10 > 0 { 1 } else { 0 };
-  let g11 = if r11 > 0 { 1 } else { 0 };
-  let g12 = if r12 > 0 { 1 } else { 0 };
+  for number in numbers {
+    r.0 -= get_bit_at(number, 11);
+    r.1 -= get_bit_at(number, 10);
+    r.2 -= get_bit_at(number, 9);
+    r.3 -= get_bit_at(number, 8);
+    r.4 -= get_bit_at(number, 7);
+    r.5 -= get_bit_at(number, 6);
+    r.6 -= get_bit_at(number, 5);
+    r.7 -= get_bit_at(number, 4);
+    r.8 -= get_bit_at(number, 3);
+    r.9 -= get_bit_at(number, 2);
+    r.10 -= get_bit_at(number, 1);
+    r.11 -= get_bit_at(number, 0);
+  }
+
+  let g0 = if r.0 > 0 { 1 } else { 0 };
+  let g1 = if r.1 > 0 { 1 } else { 0 };
+  let g2 = if r.2 > 0 { 1 } else { 0 };
+  let g3 = if r.3 > 0 { 1 } else { 0 };
+  let g4 = if r.4 > 0 { 1 } else { 0 };
+  let g5 = if r.5 > 0 { 1 } else { 0 };
+  let g6 = if r.6 > 0 { 1 } else { 0 };
+  let g7 = if r.7 > 0 { 1 } else { 0 };
+  let g8 = if r.8 > 0 { 1 } else { 0 };
+  let g9 = if r.9 > 0 { 1 } else { 0 };
+  let g10 = if r.10 > 0 { 1 } else { 0 };
+  let g11 = if r.11 > 0 { 1 } else { 0 };
 
   let gamma = format!(
     "{}{}{}{}{}{}{}{}{}{}{}{}",
-    g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12
+    g0, g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11
   );
   let gamma = i32::from_str_radix(&gamma, 2).unwrap();
 
-  let e1 = if r1 > 0 { 0 } else { 1 };
-  let e2 = if r2 > 0 { 0 } else { 1 };
-  let e3 = if r3 > 0 { 0 } else { 1 };
-  let e4 = if r4 > 0 { 0 } else { 1 };
-  let e5 = if r5 > 0 { 0 } else { 1 };
-  let e6 = if r6 > 0 { 0 } else { 1 };
-  let e7 = if r7 > 0 { 0 } else { 1 };
-  let e8 = if r8 > 0 { 0 } else { 1 };
-  let e9 = if r9 > 0 { 0 } else { 1 };
-  let e10 = if r10 > 0 { 0 } else { 1 };
-  let e11 = if r11 > 0 { 0 } else { 1 };
-  let e12 = if r12 > 0 { 0 } else { 1 };
+  let e0 = if r.0 > 0 { 0 } else { 1 };
+  let e1 = if r.1 > 0 { 0 } else { 1 };
+  let e2 = if r.2 > 0 { 0 } else { 1 };
+  let e3 = if r.3 > 0 { 0 } else { 1 };
+  let e4 = if r.4 > 0 { 0 } else { 1 };
+  let e5 = if r.5 > 0 { 0 } else { 1 };
+  let e6 = if r.6 > 0 { 0 } else { 1 };
+  let e7 = if r.7 > 0 { 0 } else { 1 };
+  let e8 = if r.8 > 0 { 0 } else { 1 };
+  let e9 = if r.9 > 0 { 0 } else { 1 };
+  let e10 = if r.10 > 0 { 0 } else { 1 };
+  let e11 = if r.11 > 0 { 0 } else { 1 };
 
   let epsilon = format!(
     "{}{}{}{}{}{}{}{}{}{}{}{}",
-    e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12
+    e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11
   );
   let epsilon = i32::from_str_radix(&epsilon, 2).unwrap();
 
